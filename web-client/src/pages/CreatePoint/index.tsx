36195a90eb020b3bf41e5402ad61ad0084f9ddb5
viniOlimpio <viniolimpio3@gmail.com>
2020-06-06 17:40:13 -0300

    a

--- web-client/src/pages/CreatePoint/index.tsx
+++ web-client/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useEffect, useState, ChangeEvent, FormEvent} from 'react';
 import './style.css';
 import logo from '../../assets/logo.svg';
 
 import { FiArrowLeft } from 'react-icons/fi';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet'
@@ -20,6 +20,8 @@ import axios from 'axios';
 
 const CreatePoint = ()=>{
 
+    const history = useHistory();
+
     //API IBGE 
     // GET UF's -> https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy={uf.any}
     // GET Munícipios da UF -> https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/distritos
@@ -34,6 +36,30 @@ const CreatePoint = ()=>{
     
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>(userLocation)
 
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
+
+    const defaultInputValues = {
+        name:'',
+        email:'',
+        whatsapp:''
+    }
+
+    const [inputData, setInputData] = useState(defaultInputValues);
+
+    //onChange Inputs!!
+    function changeInput(event: ChangeEvent<HTMLInputElement>){
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+
+        //Spread operator ...object -> pega todos os valores do objeto!!!
+        setInputData({
+             ...inputData,
+              [inputName]:inputValue 
+        });
+        console.log(inputData);
+
+    }
+
 
 
     //onChange Select de UF
@@ -44,6 +70,7 @@ const CreatePoint = ()=>{
     //onChange Select City
     function changeCity(event: ChangeEvent<HTMLSelectElement>){
         setSelectedCity(event.target.value);
+        console.log(selectedCity);
     }
 
     //Evento click no Map
@@ -52,29 +79,53 @@ const CreatePoint = ()=>{
         let longitude = event.latlng.lng;
 
         setSelectedPosition([latitude, longitude])//setando o estado da posição no mapa para o evento do click
+        console.log(selectedPosition);
+    }
+
+    //Evento click na LI de ítens
+    function clickItem(id:number){
+
+        const alreadySelected = selectedItems.findIndex(item =>{//retorna se o ítem estiver selecionado ou não
+            return item === id;
+        })
+        if(alreadySelected >= 0){//método findIndex retorna a posição de um item cujo id === à um ítem
+            
+            const filteredItems = selectedItems.filter(item =>{
+                return item !== id;//retorna todos ítens cujo id !== do id passado pelo evento
+            })
+            setSelectedItems(filteredItems);
+
+        }else{
+            //caso não encontre um index com o id passado, ele inclui no array de ítens selecionados
+            setSelectedItems( [...selectedItems, id] );
+        }
+
+        
     }
 
 
     // Buscando localização do usuário para colocar na prop center!!!
 
-    
-
     function searchLocation(){
         navigator.geolocation.getCurrentPosition((location)=>{
             let lat = location.coords.latitude;
-            let lng = location.coords.longitude;
+            let lng = location.coords.longitude;    
             setUserLocation([lat,lng]);
-
+            //caso o usuário não permita a geolocalização - default location - São Paulo
+            if(userLocation[0] === 0 && userLocation[1] === 0){
+                userLocation[0] = -23.58412603264412;
+                userLocation[1] = -46.59301757812501;
+            }
         });
     }
 
-    useEffect(searchLocation, []);
+    useEffect(searchLocation, [userLocation]);
 
     // GET UFS-----------------------------------------------------------------------------
     interface UF{
         sigla:string;
     }
-    
+        
     const [ufs, setUfs] = useState<string[]>([]);
 
     function getUfs(){
@@ -103,14 +154,14 @@ const CreatePoint = ()=>{
             return;//se o valor de uf for 0, ele não faz a pesquisa com o get
         }
 
-        axios.get<City[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/distritos`).then(response =>{
-            const res = response.data.map(city=>{
-            return city;
-            //como a interface City define o retorno/tipo, o response.data só busca o nome e id!
+        const cidade = axios.get<City[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/distritos?orderBy=nome`)
+        .then(response =>{
+            const cidade = response.data.map( city =>{
+                return city
+            });
+            setCities(cidade);
+            
         });
-
-            setCities(res);
-        })
     }    
 
     useEffect(getCities,[selectedUf]);//Quando o estado selectedUf mudar, eu chamo a função getCities!
@@ -127,7 +178,7 @@ const CreatePoint = ()=>{
     const [items, setItems] = useState<Item[]>([]);//lista de array do tipo Item
 
     function getItems(){
-        api.get('items').then((response) =>{
+        api.get('items').then( (response) =>{
 
             setItems(response.data);
 
@@ -135,6 +186,38 @@ const CreatePoint = ()=>{
     }
     useEffect(getItems, []);//inicia a função quando carregar
 
+
+    //OnSubmit
+    async function dataSubmit(event: FormEvent){
+        event.preventDefault();//previni de recarregar onSubmit!
+
+        const { name, email, whatsapp } = inputData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        
+
+        const [latitude, longitude] = selectedPosition;
+
+        const items = selectedItems;
+
+        const data = {
+            name, 
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf, 
+            items
+        }
+        
+        await api.post('points', data);
+        alert('Cadastrado com sucesso');
+        
+
+        history.push('/');
+
+    }
 
 
 
@@ -149,7 +232,7 @@ const CreatePoint = ()=>{
                 </Link>
             </header>
 
-            <form action="">
+            <form onSubmit={dataSubmit}>
                 <h1>Cadastro do <br/> ponto de Coleta </h1>
 
 
@@ -164,6 +247,7 @@ const CreatePoint = ()=>{
                             type="text"
                             name="name"
                             id="name"
+                            onChange={changeInput}
                         />
                     </div>
                     <div className="field-group">
@@ -173,6 +257,7 @@ const CreatePoint = ()=>{
                                 type="text"
                                 name="email"
                                 id="email"
+                                onChange={changeInput}
                             />
                         </div>
 
@@ -182,6 +267,7 @@ const CreatePoint = ()=>{
                                 type="text"
                                 name="whatsapp"
                                 id="whatsapp"
+                                onChange={changeInput}
                             />
                         </div>
                     </div>
@@ -233,10 +319,20 @@ const CreatePoint = ()=>{
                     </legend>
                     <ul className="items-grid">
                         {items.map(item =>(//JSX sempre vai por volta de Parênteses
-                            <li key={item.id}>
+
+                            //Sempre que quiser passar um função que tem parâmetros, sem chamá-la diretamente:
+                            // <htmlElement onEvent={()=> funcao(params)}>  !!!!
+                            <li 
+                                onClick={() => clickItem(item.id)} 
+                                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                                key={item.id}
+                            >
+
                                 <img src={item.image_url} alt={item.title}/>
                                 <span>{item.title}</span>
+
                             </li>
+
                         ))}
                     </ul>
                 </fieldset>
